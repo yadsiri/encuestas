8856f28bf4bacca13778baecc71e88fec940ad2a
yadsiri <168568076+yadsiri@users.noreply.github.com>
2024-11-17 10:39:52 -0600

    Delete resources directory

--- resources/views/encuestas/show.blade.php
+++
@@ -1,9 +0,0 @@
-<!-- resources/views/encuestas/show.blade.php -->
-@extends('layouts.app')
-
-@section('content')
-    <h1>{{ $encuesta->titulo }}</h1>
-    <p>{{ $encuesta->descripcion }}</p>
-
-    <a href="{{ route('encuestas.index') }}">Volver a la lista</a>
-@endsection
